47750cf2786a56bc5e2f7d4839964b5ba82490a4
Maxim <m.akimov@shininvest.ru>
2026-02-10 16:44:22 +0500

    1

--- src/main/java/com/makimov/retail/view/warehouse/WarehouseListView.java
+++ src/main/java/com/makimov/retail/view/warehouse/WarehouseListView.java
@@ -2,7 +2,9 @@ package com.makimov.retail.view.warehouse;
 
 import com.makimov.retail.entity.Warehouse;
 import com.makimov.retail.view.main.MainView;
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.kit.component.button.JmixButton;
 import io.jmix.flowui.view.*;
 
 
@@ -12,4 +14,9 @@ import io.jmix.flowui.view.*;
 @LookupComponent("warehousesDataGrid")
 @DialogMode(width = "64em")
 public class WarehouseListView extends StandardListView<Warehouse> {
+    @Subscribe(id = "loadingButton", subject = "clickListener")
+    public void onLoadingButtonClick(final ClickEvent<JmixButton> event) {
+        System.out.println("start loading");
+
+    }
 }
